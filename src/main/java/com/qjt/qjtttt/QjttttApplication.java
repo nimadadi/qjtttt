ec38763eac522b26e01e2faf9ba5d1552c6d3f6f
caowei <463136003@qq.com>
2020-05-07 16:21:13 +0800

    打包

--- src/main/java/com/qjt/qjtttt/QjttttApplication.java
+++ src/main/java/com/qjt/qjtttt/QjttttApplication.java
@@ -2,12 +2,19 @@ package com.qjt.qjtttt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QjttttApplication {
+public class QjttttApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(QjttttApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(QjttttApplication.class);
     }
 
 }
