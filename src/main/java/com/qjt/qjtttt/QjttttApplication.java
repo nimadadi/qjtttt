e88634c097a85eaefb502ef5edaf7b202fae6224
caowei <463136003@qq.com>
2020-08-04 15:53:33 +0800

    完善mybatis 1

--- src/main/java/com/qjt/qjtttt/QjttttApplication.java
+++ src/main/java/com/qjt/qjtttt/QjttttApplication.java
@@ -1,9 +1,11 @@
 package com.qjt.qjtttt;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qjt.qjtttt.common.dao")
 public class QjttttApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/qjt/qjtttt/common/dao/UserMapper.java
+++ src/main/java/com/qjt/qjtttt/common/dao/UserMapper.java
@@ -2,8 +2,17 @@ package com.qjt.qjtttt.common.dao;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.qjt.qjtttt.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
+    @Select("select name from t_user")
+    List<User> selectName();
+
+    List<User> selectUsername();
 
 }
